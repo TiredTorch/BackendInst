3c0904b56f73418a9e2827b31834ab1fc8aa7533
Viktor <viktor.ivanov@extrawest.com>
2022-11-23 14:23:15 +0200

    lr8-9

--- Program.cs
+++ Program.cs
@@ -1,23 +1,29 @@
-using WebApplication5;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession();
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages(options => options.RootDirectory = "/Pages");
 
 var app = builder.Build();
 
-app.UseSession();
-
-app.UseMiddleware<ErrorHandlerMiddleware>();
-app.UseMiddleware<WebSiteMiddleware>();
-//app.UseMiddleware<LogBadRequestsMiddleware>();
-
-app.Use(async (context, next) =>
+app.MapRazorPages();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    await next.Invoke();
-    if (context.Response.StatusCode == 404)
-        await context.Response.WriteAsync("Page not found!");
-});
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
