f9f51f7984fc32e31a1d6c2994b753c5f2cf4428
Viktor <viktor.ivanov@extrawest.com>
2022-11-29 15:05:16 +0200

    lr12_1

--- Program.cs
+++ Program.cs
@@ -1,27 +1,80 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Extensions.Hosting;
+using System.Reflection.Metadata;
+using WebApplication13;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+//https://learn.microsoft.com/en-us/ef/core/get-started/overview/first-app?tabs=visual-studio
 
-var app = builder.Build();
+using var db = new UserContext();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// Note: This sample requires the database to be created before running.
+Console.WriteLine($"Database path: {db.DbPath}.");
+
+// Create
+Console.WriteLine("Inserting a new user");
+var userFirst = new User
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    FirstName = "Vitya",
+    LastName = "Ivanov",
+    Age = 22
+};
+var userSecond = new User
+{
+    FirstName = "Yaroslav",
+    LastName = "Antonov",
+    Age = 33
+};
+var userThird = new User
+{
+    FirstName = "Rayan",
+    LastName = "Gosling",
+    Age = 15
+};
+
+db.Users.Add(userFirst);
+db.Users.Add(userSecond);
+db.Users.Add(userThird);
+db.SaveChanges();
+
+// Read
+Console.WriteLine("Querying for a users");
+var users = db.Users
+    .OrderBy(b => b.Id);
+
+Console.WriteLine("Num of users: ", users.Count<User>());
+foreach (var u in users)
+{
+    Console.WriteLine(u.ToString());
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+Console.WriteLine("Getting first user");
+var user = users.First();
+Console.WriteLine(user.ToString());
 
-app.UseRouting();
+// Update
+Console.WriteLine("Updating user");
+user.Age = 20;
 
-app.UseAuthorization();
+db.SaveChanges();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+var usersUpdated = db.Users
+    .OrderBy(b => b.Id);
 
-app.Run();
+Console.WriteLine("Num of users: ", usersUpdated.Count<User>());
+foreach (var u in usersUpdated)
+{
+    Console.WriteLine(u.ToString());
+}
+
+// Delete
+Console.WriteLine("Delete user");
+db.Remove(user);
+db.SaveChanges();
+
+var usersDeleted = db.Users
+    .OrderBy(b => b.Id);
+
+Console.WriteLine("Num of users: ", usersDeleted.Count<User>());
+foreach (var u in usersUpdated)
+{
+    Console.WriteLine(u.ToString());
+}
